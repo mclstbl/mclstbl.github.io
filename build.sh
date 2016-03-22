f1440fe4bee9015c577c34df9113337681ab139e
Micaela Estabillo <micaela.estabillo@gmail.com>
2016-03-22 00:57:33 -0400

    build script should install required modules

--- build.sh
+++ build.sh
@@ -6,5 +6,12 @@ docco public/scripts/parser.js -o public/docs
 docco public/scripts/codegen.js -o public/docs
 docco public/scripts/webform.js -o public/docs
 
+# install required node modules 
+if [ ! -d node_modules ]; then 
+npm install
+npm install express
+npm install body-parser
+fi
+
 # start the server to kick off React.js app
 node server.js
