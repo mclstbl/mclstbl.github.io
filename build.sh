af80693a005aedf5a0612e647271f03174fe7099
Micaela Estabillo <micaela.estabillo@gmail.com>
2016-03-30 01:22:35 -0400

    add package dependency file for building app, adjust build script

--- build.sh
+++ build.sh
@@ -3,16 +3,26 @@
 # generate documentation HTMLs using the -d flag
 if [[ $1 == "-d" ]]; then
   for i in `ls public/scripts/*js`; do
-    docco $i -o public/docs 
+    cmd="docco $i -o public/docs"
+    # change layout of documentation to linear for printing purposes
+    if [[ $2 == "-r" ]]; then
+      cmd=$cmd" -l linear"
+    # otherwise use classic layout for web rendering
+    else
+      cmd=$cmd" -l classic"
+    fi
+    eval $cmd
   done
 fi
 
 # install required node modules 
 if [ ! -d node_modules ]; then 
 npm install
-npm install express
-npm install body-parser
 fi
 
-# start the server to kick off React.js app
+i="public/scripts/"
+
+browserify -r jison -o $i"parser.js"
+
+# for demo/testing purposes, start the server to kick off React.js app
 node server.js
